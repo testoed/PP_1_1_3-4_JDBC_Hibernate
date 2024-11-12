b4b67dcfeb9507ff85cd3d66309c8ed012bc81e8
Amogus <mkkzin@mail.ru>
2024-11-13 00:00:22 +0300

    Логи вместо sout

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,6 +3,7 @@ package jm.task.core.jdbc.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Logger;
 
 public class Util {
     private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver"; // Укажите драйвер вашей БД
@@ -10,21 +11,21 @@ public class Util {
     private static final String DB_USER = "root"; // Укажите имя пользователя
     private static final String DB_PASSWORD = "1234"; // Укажите пароль
 
+    private static final Logger logger = Logger.getLogger(Util.class.getName());
     // Метод для получения соединения
     public static Connection getConnection() {
         Connection connection = null;
+
         try {
             // Загрузка драйвера
             Class.forName(DB_DRIVER);
             // Установка соединения
             connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-            System.out.println("Соединение успешно установлено!");
+            logger.info("Соединение установлено");
         } catch (ClassNotFoundException e) {
-            System.err.println("Ошибка: драйвер не найден.");
-            e.printStackTrace();
+            logger.severe("Ошибка: драйвер не найден.");
         } catch (SQLException e) {
-            System.err.println("Ошибка при установлении соединения.");
-            e.printStackTrace();
+            logger.severe("Ошибка при установлении соединения.");
         }
         return connection;
     }
